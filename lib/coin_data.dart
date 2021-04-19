0a73dddee272e481ea672e439d0b6976c2a111c9
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-04-20 04:01:10 +0530

    Bitcoin Ticker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'FB2770A0-8B68-4C94-8A71-D360DD0E27E7';
+
+class CoinData{
+  Future getCoinData(String selectedCurrency) async{
+    Map<String, String> cryptoPrices = {};
+    for(String crypto in cryptoList){
+      String url = '$coinApiUrl/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(url);
+      String data = response.body;
+      if(response.statusCode == 200){
+        var decodedData = jsonDecode(data);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      }else{
+        print(response.statusCode);
+      }
+    }
+    return cryptoPrices;
+  }
+}
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +52,3 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
-
-class CoinData {}
